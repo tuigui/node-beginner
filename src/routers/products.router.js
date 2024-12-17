e2a73b5ef0cdc1926be205eb985b6880ed0f4761
tuigui <shenbwgttw552@gmail.com>
2024-12-17 17:20:24 +0900

    feat: 상품 목록, 상세 조회 API 구현

--- src/routers/products.router.js
+++ src/routers/products.router.js
@@ -15,9 +15,20 @@ productsRouter.post("/products", async (req, res) => {
     .json({ status: 201, message: "상품 생성에 성공했습니다.", data });
 });
 
-productsRouter.get("/products", (req, res) => {});
+productsRouter.get("/products", async (req, res) => {
+  const data = await Product.find().sort({ createdAt: "desc" }).exec();
+  return res
+    .status(200)
+    .json({ status: 200, message: "상품 목록 조회에 성공했습니다.", data });
+});
 
-productsRouter.get("/products/:id", (req, res) => {});
+productsRouter.get("/products/:id", async (req, res) => {
+  const { id } = req.params;
+  const data = await Product.findById(id).exec();
+  return res
+    .status(200)
+    .json({ status: 200, message: "상품 상세 조회에 성공했습니다.", data });
+});
 
 productsRouter.put("/products/:id", (req, res) => {});
 
