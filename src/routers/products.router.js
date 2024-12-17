e1b2cb2c4ac89195393bf2f0082eaf60ff54a66f
tuigui <shenbwgttw552@gmail.com>
2024-12-17 17:40:47 +0900

    feat: 상품 수정, 삭제 API 구현

--- src/routers/products.router.js
+++ src/routers/products.router.js
@@ -30,8 +30,44 @@ productsRouter.get("/products/:id", async (req, res) => {
     .json({ status: 200, message: "상품 상세 조회에 성공했습니다.", data });
 });
 
-productsRouter.put("/products/:id", (req, res) => {});
+productsRouter.put("/products/:id", async (req, res) => {
+  const { id } = req.params;
+  const { name, description, status, manager, password } = req.body;
+  const existedProduct = await Product.findById(id, { password: true });
+  const isPasswordMatched = password === existedProduct.password;
+  if (!isPasswordMatched) {
+    return res.status(401).json({
+      status: 401,
+      message: "비밀번호가 일치하지 않습니다.",
+    });
+  }
+  const productInfo = {
+    ...(name && { name }),
+    ...(description && { description }),
+    ...(status && { status }),
+    ...(manager && { manager }),
+  };
+  const data = await Product.findByIdAndUpdate(id, productInfo, { new: true });
+  return res
+    .status(200)
+    .json({ status: 200, message: "상품 수정에 성공했습니다.", data });
+});
 
-productsRouter.delete("/products/:id", (req, res) => {});
+productsRouter.delete("/products/:id", async (req, res) => {
+  const { id } = req.params;
+  const { password } = req.body;
+  const existedProduct = await Product.findById(id, { password: true });
+  const isPasswordMatched = password === existedProduct.password;
+  if (!isPasswordMatched) {
+    return res.status(401).json({
+      status: 401,
+      message: "비밀번호가 일치하지 않습니다.",
+    });
+  }
+  const data = await Product.findByIdAndDelete(id);
+  return res
+    .status(200)
+    .json({ status: 200, message: "상품 삭제에 성공했습니다.", data });
+});
 
 export { productsRouter };
