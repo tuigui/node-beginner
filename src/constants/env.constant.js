297ea7a3efd11ea82e8be23bda53828447c55d26
tuigui <shenbwgttw552@gmail.com>
2024-12-19 18:57:59 +0900

    되돌림

--- src/constants/env.constant.js
+++ src/constants/env.constant.js
@@ -1,5 +1,5 @@
 import "dotenv/config";
 
-export const SERVER_PORT = process.env.SERVER_PORT || 3000;
+export const SERVER_PORT = process.env.SERVER_PORT;
 export const MONGODB_URL = process.env.MONGODB_URL;
 export const MONGODB_NAME = process.env.MONGODB_NAME;
